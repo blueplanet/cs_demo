cf6d246ccf75977d66da82c1d22a22491d0e7cc4
blueplanet <erguolinge@gmail.com>
2016-03-19 19:57:48 +0900

    Add users#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources 'topics', only: %i(index show)
   resources 'nodes', only: :show
+  resources 'users', only: :show
 
   resources 'ui', only: :index
   get 'ui/:action', controller: 'ui'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160319104747) do
+ActiveRecord::Schema.define(version: 20160319105155) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,13 @@ ActiveRecord::Schema.define(version: 20160319104747) do
     t.datetime "updated_at", null: false
     t.integer  "node_id"
     t.text     "body"
+    t.integer  "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

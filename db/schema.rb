7b6cc0c7019bb8a43bdb698083c1c9e2f5f0ceaa
blueplanet <erguolinge@gmail.com>
2016-03-19 19:32:22 +0900

    Add node to topics

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160319102637) do
+ActiveRecord::Schema.define(version: 20160319103007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "nodes", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +32,7 @@ ActiveRecord::Schema.define(version: 20160319102637) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "node_id"
   end
 
 end

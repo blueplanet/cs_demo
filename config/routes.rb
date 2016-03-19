a4ee524d4a3257807e3ac3680d5eb1ac16c1baad
blueplanet <erguolinge@gmail.com>
2016-03-19 19:43:43 +0900

    Add node#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources 'topics', only: :index
+  resources 'nodes', only: :show
 
   resources 'ui', only: :index
   get 'ui/:action', controller: 'ui'
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160319103007) do
+ActiveRecord::Schema.define(version: 20160319103703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "nodes", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.text     "description"
   end
 
   create_table "posts", force: :cascade do |t|

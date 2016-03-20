dcd48b9b08a7f7646103d5ffc47a842defcda426
blueplanet <erguolinge@gmail.com>
2016-03-20 21:42:43 +0900

    Add sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find params[:id]
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    User.create! params.require(:user).permit(:name, :email)
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources 'topics', only: %i(index show)
   resources 'nodes', only: :show
-  resources 'users', only: :show
+  resources 'users', only: %i(show create)
+
+  get 'sign_up', to: 'users#new'
 
   resources 'ui', only: :index
   get 'ui/:action', controller: 'ui'
+
+  root to: 'topics#index'
 end
